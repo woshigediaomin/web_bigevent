38dac82170225e00140e1fd1c7ce8da4f9ef59f2
woshigediaomin <2454074446@qq.com>
2022-05-30 20:27:47 +0800

    完成了主页的功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,25 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter((option) => {
-    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    option.url = `http://www.liulongbin.top:3007` + option.url;
-  });
+$.ajaxPrefilter((options) => {
+  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  options.url = `http://www.liulongbin.top:3007` + options.url;
+
+  //为/my/相关接口 注入 token
+  if (options.url.includes("/my/")) {
+    options.headers = {
+      Authorization: localStorage.getItem("token")
+    }
+  }
+
+  // 不论成功还是失败，最终都会调用 complete 回调函数
+  options.complete = (res) => {
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+      //  强制清空 token
+      localStorage.removeItem("token");
+      // 强制跳转到登录页面
+      location.href = "/login.html";
+    }
+  }
+});
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+// 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+        // headers: {
+        //     Authorization: localStorage.getItem("token")
+        // },
+        success: (res) => {
+            if (res.status !== 0) return layer.msg("获取用户信息失败")
+            layer.msg("获取用户信息成功")
+            renderAvatar(res.data)
+        }
+    })
+}
+
+// 渲染用户信息
+const renderAvatar = (user) => {
+    console.log(user);
+    let uname = user.nickname || user.username
+    // 渲染欢迎语
+    $("#welcome").html(`欢迎 ${uname}`)
+    // 按需渲染头像
+    if (user.user_pic !== null) {
+        //设置图片头像
+        $(".layui-nav-img").attr("src", user.user_pic).show();
+        $(".text-avatar").hide();
+    } else {
+        //设置文本头像
+        $(".layui-nav-img").hide()
+        $(".text-avatar").html(uname[0].toUpperCase()).show()
+    }
+}
+
+// 退出登录
+$("#btnLogout").click(() => {
+    layui.layer.confirm(
+        "确定退出登录？",
+        { icon: 3, title: "" },
+        function (index) {
+            // 清空本地存储里面的 token
+            localStorage.removeItem("token");
+            // 重新跳转到登录页面
+            location.href = "/login.html";
+        }
+    );
+});
+
+getUserInfo()
